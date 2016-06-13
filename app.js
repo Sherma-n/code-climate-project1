b498e38d8968c4f140227f69d403a027256cf647
Sherma-n <ysh.sherman@gmail.com>
2016-06-13 16:26:00 +0800

    Sherman

---
+++ app.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){               //wait page load
+  // HTML Elements
+  var $rollGameBtn = $('#rollGame');        //roll btn
+  var $chips       = $('.chips');           //chips
+  var $bets       = $('.bets');             //bets
+
+  // Game Settings
+  var defaultCredits = 100;                 //default credits
+
+  // Game Container Object
+  var game = new Game(defaultCredits);      //set credits to default
+
+  // Event Listeners
+  $rollGameBtn.on('click', function(){      // clicking roll btn
+    game.clearTable();                          //clearing the table for each round
+    game.rollSound.play();                      //roll sound
+    var winners = game.winners;                 //update bet winners
+    game.rollDice();                            //roll the dices
+    game.checkWinners();                        // creates array of key names as strings
+    game.highLightWins ();                      //hightlight winning bets
+    game.payout();                              //calculating payouts
+    game.updateChips();                         //paying players with payout
+    game.checkGameWinner();                     //check if there is a game winner
+  });
+
+  $chips.on('click', function(){            //clicking on chips
+    game.coins.play();                        //coin sounds
+    var $chip   = $(this);                    //this specific chip
+    var player  = $chip.data("player");       //update player value
+    var betSize = $chip.data("value")         //get chip value
+    game.currentPlayer        = game[player]; //get current player value
+    game.currentPlayerBetSize = betSize;      //get current bets & be sizes
+  });
+
+  $bets.on('click', function(){             //clicking on bets
+    var $OE           = $(this);
+    game.coins.play();                            //Play coin sound
+    var betType       = $OE.data("bet-type");     //determine bet type
+    var currentPlayer = game.currentPlayer;       //deter which player does this belong to
+    var betSize       = game.currentPlayerBetSize;//determine current bet size
+    if (currentPlayer.credits < betSize) {        //check if player is able to bet
+      alert("Insufficient to make bet");          //can't bet man, no value
+    } else {                                      //minus player bet
+      currentPlayer.credits       -= betSize;     //update credits
+      currentPlayer.bets[betType] += betSize;     //updating credits
+      $ (this).css("background-image", "url(http://customcandyexpress.com/graphics/denom.png)");      //css on betboxes
+      $ (this).css("border", "3px solid white");                                                      //css on betboxes
+      $ (this).css("background-size", "contain");                                                     //css on betboxes
+      $ (this).css("background-repeat", "no-repeat");                                                 //css on betboxes
+    }
+  });
+
+  $('#resetGame').on('click', function(){   //resetting the game
+    location.reload();
+    console.log("game has been reset");
+  });
+
+  window.debug = game;                           //for debugging
+});
+
+// Psuedo Code
+//  1. Determine Player Values
+//    1.1 Set Player Default = 100
+//  2. Count Turns
+//    2.1 Default Count Turn = 0
+//  3. Place Wagers
+//    3.1 Get Chip Values
+//      3.1.1 For Player 1
+//      3.1.2 For Player 2
+//      3.1.3 If Chip Value > Chip
+//        3.1.3.1 If true will turn chip opaque and cannot be clicked
+//        3.1.3.2 If False, Nothing Happens
+//    3.2 Chips Drag and Drop
+//      3.2.1 On click will bring the wager to the mouse.
+//      3.2.2 On Drop will assign wager to BetBox.
+//    3.3 Deduct Chip Value From Player Value
+//      3.3.1 Deduct for Player 1
+//      3.3.2 Deduct for Player 2
+//    3.4 Sum Total Wager On Bet
+//      3.4.1 For Player 1
+//      3.4.2 For Player 2
+//    3.5 Get Sum of Wagers On Bets
+//      3.5.1 Sum For Player 1
+//      3.5.2 Sum For Player 2
+//  4. Roll Dice
+//    4.1 Random Die Values 1-6
+//    4.2 Sum both Dices
+//  5. Get Bet Wins
+//    5.1 Check for Pair Values
+//      5.1.1 If True Check if any Pair hits
+//      5.1.2 If False Skip
+//    5.2 Check if Sum of Dices Satisfies Condition for Win
+//      5.2.1 If True Highlight the BetBox
+//      5.2.1 If False Skip it
+//  6. Calculate Payout
+//    6.1 Check if Bet Wins for Bet Box
+//      6.1.1 If True, Check if wager is present
+//        6.1.1.1 If True, Proceed to Calculate Payout
+//      6.1.2 If False, Skip
+//    6.2 Calculate Payout
+//      6.2.1 Determine Wager Value & * by Payout.
+//      6.2.2 Determine if Wager is from Player 1 or 2.
+//        6.2.2.1 If Player One add payout to value of Player One.
+//        6.2.2.2 If Payer Two add Payout to value of Player 2.
+//  7. Add Payout to Player Value
+//    7.1 Add Payout to Player 1 & calculate total value
+//    7.2 Add Payout to Player 2 & Calculate total Value
+//    7.3 Alert to show round Summary
+//      7.3.1 Get sum of Payouts
+//      7.3.2 Get sum of Wagers
+//      7.3.3 Get Total Sum
+//    7.4 Reset Wager for board to be 0
+//  8. Determine Winner
+//    8.1 Check if Player Value > 0.
+//      8.1.1 If true, nothing Happens
+//      8.1.2 If False, will check if player 1 or 2 = 0.
+//        8.1.2.1 If player 1 = 0, Player 2 Wins
+//        8.1.2.2 if Player 2 = 0 , Player 1 Wins
+//        8.1.2.3 Else, Nothing Happens
+//    8.2 Check if Round Number >= 5.
+//      8.2.1 If False, Nothing Happens
+//      8.2.2 If True, Check if player value 1 > player value 2.
+//        8.2.2.1 If True, Player 1 Wins, Display player value for both. highlight player 1
+//        8.2.2.2 If False, Player 2 Wins, Display player value for both, highlight player 2
+//        8.2.2.3 Else, Game Draw, Will Display Value for both players.
+//  9. Reset the Game
+//    9.1 Reload the page.
+// Event Listners
+//  On Click Wagers
+//  Drag Wagers to BetBoxes
+//  Unclick Wagers
+//  Roll button
+//  Closing Round Summary Alert
+//  Reset Button for Page
---
+++ objects.js
@@ -0,0 +1,248 @@
+// ====================
+// Payout Ratio Object - Payout for Each Bet
+// ====================
+var payoutRatio = {
+  big:    2,
+  even:   2,
+  odd:    2,
+  small:  2,
+  pair1:  30,
+  pair2:  30,
+  pair3:  30,
+  pair4:  30,
+  pair5:  30,
+  pair6:  30,
+  bet2:   30,
+  bet3:   15,
+  bet4:   7,
+  bet5:   3,
+  bet6:   2,
+  bet7:   3,
+  bet8:   2,
+  bet9:   3,
+  bet10:  7,
+  bet11:  15,
+  bet12:  30,
+};
+
+// ====================
+// Bet Object - Array to store Wagers for each Bet
+// ====================
+var Bet = function() {
+  this.big   = 0;
+  this.even  = 0;
+  this.odd   = 0;
+  this.small = 0;
+  this.pair1 = 0;
+  this.pair2 = 0;
+  this.pair3 = 0;
+  this.pair4 = 0;
+  this.pair5 = 0;
+  this.pair6 = 0;
+  this.bet2  = 0;
+  this.bet3  = 0;
+  this.bet4  = 0;
+  this.bet5  = 0;
+  this.bet6  = 0;
+  this.bet7  = 0;
+  this.bet8  = 0;
+  this.bet9  = 0;
+  this.bet10 = 0;
+  this.bet11 = 0;
+  this.bet12 = 0;
+};
+
+// ====================
+// Player Object - Player values
+// ====================
+var Player = function(name, credits){
+  this.name    = name;
+  this.credits = credits;
+  this.bets    = new Bet();
+};
+
+// ====================
+// Game Object - Game Objects and Functions
+// ====================
+var Game = function(defaultCredits) {
+  this.turn                 = 0;                                             //Check Game Winner
+  this.player1              = new Player("Denis", defaultCredits);           //Payout & Check Game Winner
+  this.player2              = new Player("Jens", defaultCredits);            //Payout & Check Game Winner
+  this.dice                 = {};                                            //rollDice
+  this.winners              = [];                                            //Array of strings that matches the name of bets
+  this.currentPlayer        = 'Denis';                                       //Player that just clicked on Chips
+  this.currentPlayerBetSize = 0;                                             //Player bet that just selected CHips
+
+  // ====================
+  // Sounds
+  // ====================
+   this.coins = new buzz.sound( "./Money-sound-insert-coin.mp3", { preload: true, loop: false });
+
+   this.rollSound = new buzz.sound( "./Knock-on-door.mp3", { preload: true, loop: false });
+
+   this.winSound = new buzz.sound( "./Happy-music.mp3", { preload: true, loop: false });
+
+// ====================
+// Roll Dice
+// ====================
+   this.rollImage = function (diceRoll, diceID) {
+      switch (diceRoll) {
+        case 1:
+          $(diceID).css("background-image", "url(http://www.clipartkid.com/images/170/dice-faces-clipart-1-9-reaching-teachers-labd0b-clipart.png)");
+          break;
+        case 2:
+          $(diceID).css("background-image", "url(http://i363.photobucket.com/albums/oo79/fizzgig2k4/dice%20face%20images/lego2dice-1-2.jpg)");
+          break;
+        case 3:
+          $(diceID).css("background-image", "url(http://liarsdice.co/static/face3.png)");
+          break;
+        case 4:
+          $(diceID).css("background-image", "url(http://dobbelsteen.virtuworld.net/img/4c.gif)");
+          break;
+        case 5:
+          $(diceID).css("background-image", "url(https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png)");
+          break;
+        case 6:
+          $(diceID).css("background-image", "url(http://www.zonkthegame.com/img/6.png)");
+          break;
+      };
+   };
+
+   this.diceImages = function (dice1, dice2, dice1Roll, dice2Roll) {                    //Sets background & choosing an image.
+      $ (dice1).css("background-size", "contain");
+      $ (dice1).css("background-repeat","no-repeat");
+      $ (dice2).css("background-size", "contain");
+      $ (dice2).css("background-repeat","no-repeat");
+      this.rollImage (dice1Roll, dice1);
+      this.rollImage (dice2Roll, dice2);
+   };
+
+  this.rollDice = function() {                                                         //Rolls Both Dices For Random Number Between 1 to 6
+      this.dice.dice1         = Math.ceil( Math.random() * 6 );                        //Rolls Dice 1
+      this.dice.dice2         = Math.ceil( Math.random() * 6 );                        //Rolls Dice 2
+      this.dice.total         = this.dice.dice1 + this.dice.dice2;                     //Sum of Dice 1 & 2
+      this.turn               ++;                                                      //Turn Count +1
+      this.diceImages('#dieOne', '#dieTwo', this.dice.dice1, this.dice.dice2);
+  };
+
+// ====================
+// Check Round Winners
+// ====================
+  this.pairWins = function (dice1, dice2) {                                    //Push pairs into win arrays
+    if (dice1 === dice2)                        { this.winners.push("pair" + dice1);}
+    else                                        {console.log("No Pairs")};
+  }
+
+  this.diceWins = function (diceTotal)    {                                      //Push dices into win arrays
+    if (diceTotal > 1)                          { this.winners.push("bets" + diceTotal);}
+    else                                        {console.log("Error no dice rolls")};
+  }
+
+  this.evenWins = function (diceTotal)    {                                      //Push even and odds into win arrays
+    if ((diceTotal%2) === 0)                    { this.winners.push("even"); }
+    else                                        { this.winners.push("odd"); }
+  };
+
+  this.bigWins = function (diceTotal)     {                                      //Push big and small into win arrays
+    if (diceTotal >= 7)                         { this.winners.push("big"); }
+    else                                        { this.winners.push("small"); }
+  };
+
+
+  this.checkWinners = function() {                                                              //Check For Winning bets of each round(not game winner)
+    this.pairWins (this.dice.dice1, this.dice.dice2);
+    this.diceWins (this.dice.total);
+    this.evenWins (this.dice.total);
+    this.bigWins  (this.dice.total);
+    console.log(this.dice.dice1 + " " + this.dice.dice2 + " " + this.dice.total);               //Logging dice roll returns
+    console.log(this.winners)                                                                   //check winning array
+  };
+
+  this.highLightWins = function () {                                         //Hightling "BetBoxes that are in the winng array"
+    var high         = this.winners;                                         //Pushing to winning array
+    for (i = 0 ; i < high.length ; i++) {                                    //Looping through the array
+      var light      = high[i]
+      $('#'+ light).css("background-color", "white");                        //css effect
+      $('#'+ light).css("border", "3px solid white");                        //css effect
+      $('#'+ light).css("background-image", "url(http://awardswriters.com/wp-content/uploads/Winner-stamp1-e1428843025115.jpg)");
+      $('#'+ light).css("background-size", "contain");                       //css effect
+      $('#'+ light).css("background-repeat", "no-repeat");                   //css effect
+    }
+  };
+
+// ====================
+// Payouts
+// ====================
+  this.payout = function () {                                                //Depositing Payout for wins to each player
+    this.calculatePayout("player1", payoutRatio);                            //Depositing Player1
+    this.calculatePayout("player2", payoutRatio);                            //Depositing Player2
+    this.player1.bets = [];                                                  //Round bets for p1
+    this.player2.bets = [];                                                  //Round bets for p2
+  };
+
+  this.calculatePayout = function (player, payoutRatio) {                    //Calculating amount for payout
+    var player                  = this[player];                                 //Determine Player
+    for (var betName in player.bets) {                                       //For loop to get each bet
+      var amount                = player.bets[betName];                         //Amount Player Bet in
+      var winnerIndex           = this.winners.indexOf(betName);                //Winning bets this round
+      if (amount && winnerIndex >= 0) {                                      //if bet made + bet won
+        ratio                   = payoutRatio[betName];                         //Calculate payout
+        player.credits          += amount * ratio;                              //add payout to player
+        player.bets[betName]    = 0;                                            //Reset payer bets
+      }
+    };
+  };
+
+  this.clearTable = function() {                                             //clearing the table at the end of each round
+    this.dice                 = {};                                          //reset dices
+    this.winners              = [];                                          //reset winners
+    this.currentPlayer        = ' ';                                         //reset playing player
+    this.currentPlayerBetSize = 0;                                           //reset bets
+    $ (".bets").css("background", "green");                                  //reset gameboard css
+  };
+
+// ====================
+// Noty
+// ====================                                                    //Noty - message for winners
+this.messageOnWin = function (Player1, Player2, Player1Credits, Player2Credits) { noty ( {
+  text:       Player1 + " WINS!"  + Player1 + "Credits:" + Player1Credits + " ." + Player2 + "Credits:" + Player2Credits,
+  animation: {
+    open:     'animated bounceInLeft',       // Animate.css class names
+    open:     {height: 'toggle'},            //effect for fade in
+    close:    'animated bounceOutLeft',      // Animate.css class names
+    easing:   'swing',                       // unavailable - no need
+    speed:    300                            // unavailable - no need
+    }
+  });
+  this.winSound.play();
+};
+// ====================
+// Check Game Winners
+// ====================
+  this.playerMoreChips = function (Player1Credits, Player2Credits) {
+    if          (Player1Credits > Player2Credits) { this.messageOnWin("Player1", "Player2", Player1Credits,Player2Credits); }
+    else if     (Player1Credits < Player2Credits) { this.messageOnWin("Player2", "Player1", Player2Credits,Player1Credits); }
+    else                                          { this.messageOnWin( "Player1&2", "Player2" , Player2Credits,Player1Credits);}
+  };
+
+  this.checkRoundNumber = function (Player1Credits, Player2Credits) {
+    if          (this.turn >=3)                   { this.playerMoreChips(Player1Credits, Player2Credits);}
+    else                                          { console.log("No Winner Yet!") };
+  }
+
+  this.checkLoser = function (Player1Credits, Player2Credits) {
+    if          (Player2Credits <= 0)             { this.messageOnWin("Player1", "Player2", Player1Credits,Player2Credits);}
+    else if     (Player1Credits <= 0)             { this.messageOnWin("Player2", "Player1", Player2Credits,Player1Credits);}
+    else                                          { console.log("No Loser Yet!");}
+  };
+
+  this.checkGameWinner = function () {                                      //checking for a game winner
+    this.checkRoundNumber(this.player1.credits,this.player2.credits);
+    this.checkLoser(this.player1.credits,this.player2.credits);
+    };
+
+  this.updateChips = function () {                                          //Updating player values (with Payout - bet)
+    $('#playerOneValue').text(this.player1.credits);                        //P1
+    $('#playerTwoValue').text(this.player2.credits);                        //P2
+  };
+};//Game Objects & Functions END
